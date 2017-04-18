233228235c308d79b8e34a57ba3be8f78dfb31de
Troy Stribling <troy.stribling@gmail.com>
2017-04-18 09:31:14 -0700

    created project

---
+++ PracticeSessions/PracticeSessions/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PracticeSessions
+//
+//  Created by Troy Stribling on 4/18/17.
+//  Copyright © 2017 Troy Stribling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
